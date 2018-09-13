b8aa4d9247690458f72107120521a140d9fe345e
anpatel21 <anpatel21@HS-MATH2-18.gvsd.org>
2018-09-13 13:38:09 -0400

    test

---
+++ Main.java
@@ -0,0 +1,27 @@
+public class Main 
+{
+
+	/*
+	 * Store every quantity in a variable.
+	 * Write a program which will display a person's name.
+	 * Write a program which will calculate the area of a rectangle.
+	 * Write a program which will calculate the area and circumference of a circle.
+	 * 
+	 */ 
+	
+	public static void main(String[] args) 
+	{
+		String name = "Anuj";
+		System.out.println(name);
+		
+		int length = 1;
+		int height = 2;
+		int rectangleArea = length * height;
+		
+		double pi = 3.14;
+		int radius = 1;
+		double circleArea = pi * radius * radius;
+		double circumference = 2 * pi * radius;
+	}
+
+}
